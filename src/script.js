34d2a10742518023a23513d01cd4f94c6857cdf1
HOoMAN <h.h.0.6.6.9.1384@gmail.com>
2024-09-25 01:49:12 +0330

    nav and hero section

---
+++ src/script.js
@@ -0,0 +1,19 @@
+// let $ = document;
+// // Slider Functionality
+// const slider = $.getElementById("slider");
+// const slides = slider.children.length;
+// let currentIndex = 0;
+
+// $.getElementById("next").addEventListener("click", () => {
+//   currentIndex = (currentIndex + 1) % slides;
+//   updateSlider();
+// });
+
+// $.getElementById("prev").addEventListener("click", () => {
+//   currentIndex = (currentIndex - 1 + slides) % slides;
+//   updateSlider();
+// });
+
+// function updateSlider() {
+//   slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+// }
