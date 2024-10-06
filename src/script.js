dc5adc5f14cdef5dbc53f79f14c63a5cfce4d8f1
HOoMAN <h.h.0.6.6.9.1384@gmail.com>
2024-10-07 00:48:46 +0330

    about doctor and hero sec update

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ let servicesIcon = $.querySelector("#servicesIcon");
 
 let beutyServ = $.querySelector("#beutyServ");
 let beutyServBar = $.querySelector("#beutyServBar");
+let beutyServBarIcon = $.querySelector("#beutyServBarIcon");
 let beutyServicesToggle = false;
 
 let aboutLi = $.querySelector("#aboutLi");
@@ -30,99 +31,6 @@ let botoxCardH3Second = $.getElementById("botoxCardH3Second");
 
 //پایان تعریف متغیر ها
 
-// // Toggle the servicesBar when servicesLi is clicked
-// servicesLi.addEventListener("click", function (event) {
-//   // Stop event propagation to prevent the document body click from immediately hiding the servicesBar
-//   event.stopPropagation();
-
-//   // If aboutBar is visible, hide it
-//   if (aboutBarToggle) {
-//     aboutBar.classList = "hidden";
-//     aboutIcon.classList = "size-4 inline";
-//     aboutBarToggle = false;
-//   }
-
-//   if (!servicesBarToggle) {
-//     // Show servicesBar and rotate icon
-//     servicesBar.classList =
-//       "text-white absolute w-36 bg-blue-800 bg-opacity-80 -right-[30px] rounded-2xl flex flex-col items-center leading-8 p-3 m-2 hover:bg-white hover:text-blue-800";
-//     servicesIcon.classList = "size-4 inline rotate-180";
-//     servicesBarToggle = true;
-//   } else {
-//     // Hide servicesBar and reset icon
-//     servicesBar.classList = "hidden";
-//     servicesIcon.classList = "size-4 inline";
-//     servicesBarToggle = false;
-//   }
-// });
-
-// // Toggle the aboutBar when aboutLi is clicked
-// aboutLi.addEventListener("click", function (event) {
-//   // Stop event propagation to prevent the document body click from immediately hiding the aboutBar
-//   event.stopPropagation();
-
-//   // If servicesBar is visible, hide it
-//   if (servicesBarToggle) {
-//     servicesBar.classList = "hidden";
-//     servicesIcon.classList = "size-4 inline";
-//     servicesBarToggle = false;
-//   }
-
-//   if (!aboutBarToggle) {
-//     // Show aboutBar and rotate icon
-//     aboutBar.classList =
-//       "text-white absolute w-36 bg-blue-800 bg-opacity-80 -right-[30px] rounded-2xl flex flex-col items-center leading-8 p-3 m-2 hover:bg-white hover:text-blue-800";
-//     aboutIcon.classList = "size-4 inline rotate-180";
-//     aboutBarToggle = true;
-//   } else {
-//     // Hide aboutBar and reset icon
-//     aboutBar.classList = "hidden";
-//     aboutIcon.classList = "size-4 inline";
-//     aboutBarToggle = false;
-//   }
-// });
-
-// // Add a click listener to the document body to hide the servicesBar and aboutBar when clicking outside of them
-// $.body.addEventListener("click", function (event) {
-//   // Check if the click is outside the servicesBar and servicesLi
-//   if (
-//     servicesBarToggle &&
-//     !servicesBar.contains(event.target) &&
-//     !servicesLi.contains(event.target)
-//   ) {
-//     // Hide servicesBar and reset icon
-//     servicesBar.classList = "hidden";
-//     servicesIcon.classList = "size-4 inline";
-//     servicesBarToggle = false;
-//   }
-
-//   // Check if the click is outside the aboutBar and aboutLi
-//   if (
-//     aboutBarToggle &&
-//     !aboutBar.contains(event.target) &&
-//     !aboutLi.contains(event.target)
-//   ) {
-//     // Hide aboutBar and reset icon
-//     aboutBar.classList = "hidden";
-//     aboutIcon.classList = "size-4 inline";
-//     aboutBarToggle = false;
-//   }
-// });
-
-// //باز شدن بار خدمات زیبایی
-// beutyServ.addEventListener("click", function () {
-//   if (!beutyServicesToggle) {
-//     beutyServBar.classList =
-//       "flex rounded-2xl absolute right-[145px] top-10 w-36 bg-blue-800 text-white hover:bg-white bg-opacity-80 hover:text-blue-700 flex-col items-center leading-8 p-3";
-//     servicesBarIcon.classList = "size-4 inline rotate-90";
-//     beutyServicesToggle = true;
-//   } else {
-//     beutyServBar.classList = "hidden";
-//     servicesBarIcon.classList = "size-4 inline";
-//     beutyServicesToggle = false;
-//   }
-// });
-
 // نمایش و مخفی‌سازی servicesBar وقتی روی servicesLi کلیک می‌شود
 servicesLi.addEventListener("click", function (event) {
   event.stopPropagation();
@@ -144,7 +52,7 @@ servicesLi.addEventListener("click", function (event) {
     servicesIcon.classList.remove("rotate-180");
     servicesBarToggle = false;
     beutyServBar.classList.add("hidden"); // مخفی کردن beutyServBar در صورت بسته شدن servicesBar
-    beutyServToggle = false;
+    beutyServicesToggle = false;
   }
 });
 
@@ -160,12 +68,14 @@ beutyServ.addEventListener("click", function (event) {
   }
 
   // نمایش یا مخفی کردن beutyServBar
-  if (!beutyServToggle) {
+  if (!beutyServicesToggle) {
     beutyServBar.classList.remove("hidden");
-    beutyServToggle = true;
+    beutyServBarIcon.classList.add("rotate-90");
+    beutyServicesToggle = true;
   } else {
     beutyServBar.classList.add("hidden");
-    beutyServToggle = false;
+    beutyServBarIcon.classList.remove("rotate-90");
+    beutyServicesToggle = false;
   }
 });
 
@@ -179,7 +89,8 @@ aboutLi.addEventListener("click", function (event) {
     servicesIcon.classList.remove("rotate-180");
     servicesBarToggle = false;
     beutyServBar.classList.add("hidden"); // مخفی کردن beutyServBar در صورت بسته شدن servicesBar
-    beutyServToggle = false;
+    beutyServBarIcon.classList.remove("rotate-90");
+    beutyServicesToggle = false;
   }
 
   // نمایش یا مخفی کردن aboutBar
@@ -206,7 +117,8 @@ document.body.addEventListener("click", function (event) {
     servicesIcon.classList.remove("rotate-180");
     servicesBarToggle = false;
     beutyServBar.classList.add("hidden");
-    beutyServToggle = false;
+    beutyServBarIcon.classList.remove("rotate-90");
+    beutyServicesToggle = false;
   }
 
   // اگر بیرون از aboutBar و aboutLi کلیک شد
